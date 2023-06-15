eb60bcc43d4cf9f3ad6b412688daa107ad1e4ecd
camusfls <imad@imad.im>
2023-06-15 14:59:50 -0700

    imad

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Using puts to write a sentence followed by new line
+ *
+ * Return: Always0.
+ */
+int main(void)
+{
+	puts ("Programming is like building a multilingual puzzle");
+	return(0);
+}
