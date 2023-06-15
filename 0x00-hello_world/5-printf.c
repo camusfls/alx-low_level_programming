d08e49deee02c40270a380b5c1fd6874133501ad
camusfls <imad@imad.im>
2023-06-15 15:18:34 -0700

    imad

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ * File: 4-puts.c
+ * Auth: imad jahfel
+ */
+#include <stdio.h>
+/**
+ * main -  puts "to write a sentence followed by new line.
+ *
+ *
+ * Return: Always0.
+ */
+int main(void)
+{
+        printf("\"with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
+~                   
