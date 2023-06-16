04909a411b4d3f0c7bc3e935f0d2fec1956789e7
camusfls <imad@imad.im>
2023-06-16 16:51:18 -0700

    imad

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+/*
+ * File: 6-print_numberz.c
+ * Auth: imad
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Print single digit numbers of base 10 starting from 0
+ *
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
