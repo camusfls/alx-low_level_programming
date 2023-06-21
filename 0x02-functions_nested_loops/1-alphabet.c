8e1f0f2e5a0c6f365f26d58cadb0e9ca5a19ac53
camusfls <imad@imad.im>
2023-06-20 17:42:38 -0700

    imad

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet - Prints the alphabet
+ * print_alphabet - Prints the alphabet in lowercase.
  *
- * Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
