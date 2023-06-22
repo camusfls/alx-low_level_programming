97d29222b27736beb1ea8346d3b68cb37a743389
camusfls <imad@imad.im>
2023-06-21 17:16:25 -0700

    imad

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,17 +4,12 @@
  * lowercase or if a higher case
  * @c: The character to be checked.
  *
- * Return: 1 for lowercase characte. 0 for higher case
+ * Return: 1 for lowercase characte. 0 for higher}
  */
 int _islower(int c)
 {
-	if (c >= 97 && c <= 122)
-	{
+	if (c >= 'a' && c <= 'z')
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
-	_putchar('\n'),
 }
