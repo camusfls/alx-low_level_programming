a07376709c88b1d9b38972d7be023aa8d9baa760
camusfls <imad@imad.im>
2023-06-21 17:04:48 -0700

    imad

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * çislower - Shows if the input is a 
+ * lowercase or if a higher case
+ *
+ * 
+ * Return: 1 for lowercase characte. 0 for higher case
+ */
+int _islower (int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n'),
+}
