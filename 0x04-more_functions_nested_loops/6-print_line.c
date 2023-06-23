b9c344641f5ab3237d6a065bd8b8462374bae24e
camusfls <imad@imad.im>
2023-06-23 15:35:37 -0700

    imad

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#incude "main.h"
+
+/**
+ * print_line - print a line using_
+ *
+ *@n: number of _ composed the line
+ */
+
+void print_line(int n)
+{
+	int num_lines;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (num_lines = 1; num_lines <= n; num_lines++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
